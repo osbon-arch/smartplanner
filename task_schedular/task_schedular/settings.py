86bd7d42ed58f833c737ce49be1b762e0b1690c3
osbon mwangori <71443732+osbon-arch@users.noreply.github.com>
2025-05-29 12:48:28 +0300

    Update settings.py

--- task_schedular/task_schedular/settings.py
+++ task_schedular/task_schedular/settings.py
@@ -67,6 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                 'task_schedular.context_processors.version_context',
             ],
         },
     },
